51b94b37ef70eb300784c11644803afb0a737fe7
U-PAYPALCORP\sichuang <sichuang@L-SJN-21005571.paypalcorp.com>
2016-12-06 15:44:36 -0800

    add generic question handling functionality

--- scripts/Weighted_Classifier.py
+++ scripts/Weighted_Classifier.py
@@ -1,42 +1,34 @@
-
-# coding: utf-8
-
-# In[10]:
-
+import os
 import sys
 import pickle
 from textblob import TextBlob
+import numpy as np
+import pandas as pd
+from openpyxl import load_workbook
 
-
-# In[11]:
+faq = pd.read_excel(open('/Users/ravkrishnan/krakenapp/scripts/FAQ_questions_Dump.xlsx', 'rb'), sheetname='Export Worksheet')
 
 def split_into_lemmas(comment):
     words = TextBlob(comment).words
     # for each word, take its "base form" = lemma 
     return [word.lemma for word in words]
 
-
-# In[12]:
-
 def tfidf_trans(bow_transformer, tfidf_transformer, comments):
     comment_bow = bow_transformer.transform(comments)
     comment_tfidf = tfidf_transformer.transform(comment_bow)
     return comment_tfidf
 
-
-# In[13]:
-
 with open('/Users/ravkrishnan/krakenapp/scripts/comment_classifier.pkl', 'rb') as f:
     bow_transformer, tfidf_transformer, cl = pickle.load(f)
-
-
-# In[14]:
 
 #comment = 'Did not receive my TV.'
 comment = str(sys.argv)[1]
 
-
-# In[15]:
-
-print(cl.predict(tfidf_trans(bow_transformer, tfidf_transformer, [comment]))[0])
-
+ind = None
+if sum(faq['TITLE'].str.contains(comment, case=False)) > 0:
+    ind = np.where([faq['TITLE'].str.contains(comment, case=False)])[0][0]
+    
+if ind is not None:
+    print(faq['CONTENT'][ind])
+else:
+    print(cl.predict(tfidf_trans(bow_transformer, tfidf_transformer, [comment]))[0])
