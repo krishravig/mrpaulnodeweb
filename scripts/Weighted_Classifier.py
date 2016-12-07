88300cce95c4bc31b8f0a5077ed2a1a2cfc196f4
U-PAYPALCORP\sichuang <sichuang@L-SJN-21005571.paypalcorp.com>
2016-12-06 16:32:32 -0800

    corrected error

--- scripts/Weighted_Classifier.py
+++ scripts/Weighted_Classifier.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 from openpyxl import load_workbook
 
-faq = pd.read_excel(open('/Users/ravkrishnan/krakenapp/scripts/FAQ_questions_Dump.xlsx', 'rb'), sheetname='Export Worksheet')
+faq = pd.read_excel(open('../FAQ_questions_Dump.xlsx', 'rb'), sheetname='Export Worksheet')
 
 def split_into_lemmas(comment):
     words = TextBlob(comment).words
@@ -18,11 +18,11 @@ def tfidf_trans(bow_transformer, tfidf_transformer, comments):
     comment_tfidf = tfidf_transformer.transform(comment_bow)
     return comment_tfidf
 
-with open('/Users/ravkrishnan/krakenapp/scripts/comment_classifier.pkl', 'rb') as f:
+with open('comment_classifier.pkl', 'rb') as f:
     bow_transformer, tfidf_transformer, cl = pickle.load(f)
 
 #comment = 'Did not receive my TV.'
-comment = str(sys.argv)[1]
+comment = str(sys.argv[1])
 
 ind = None
 if sum(faq['TITLE'].str.contains(comment, case=False)) > 0:
